8aa05aaa4af651f3e4617d799c10261668064e29
Sajal <sajal.kumar2003@gmail.com>
2024-12-20 01:34:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ const App = () => {
         } 
       />
       <Route 
-        path="/gpa-calculator" 
+        path="/cgpa-calculator" 
         element={
           <ProtectedRoute>
             <VTUCalculator />
